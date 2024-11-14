713930050e9ebcb214a6b044073c54b19179603b
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2024-11-14 14:04:40 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,9 @@
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-
+ new CookiesEuBanner(function () {
+        // Your code here
+    });
 (function($) {
 
 	skel.breakpoints({
